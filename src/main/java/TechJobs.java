c67757f6d40c36d96aef99f6a957681a2f1ced5d
MuntoniFrancesca <francesca.muntoni@live.com>
2023-08-15 14:23:49 -0500

    Finished task 1, test doesn't pass

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -119,7 +119,18 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-
-        System.out.println("printJobs is not implemented yet");
+        if (someJobs.size() == 0) {
+            System.out.println("No Results");
+        } else {
+        for (int i =0; i < JobData.findAll().size(); i++) {
+            System.out.println("*****");
+            for(Map.Entry<String, String> entry: someJobs.get(i).entrySet()) {
+                String key = entry.getKey();
+                String value = entry.getValue();
+                System.out.println(key + ": " + value);
+            }
+            System.out.println("***** \n");
+        }
+        }
     }
 }
